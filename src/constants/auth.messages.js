e214266d807e71fed4a9fc8dfb780560b6b0c50d
MimHe <mimhe1381@gmail.com>
2025-01-08 12:31:57 +0330

    Add /auth/registration parts

---
+++ src/constants/auth.messages.js
@@ -0,0 +1,4 @@
+module.exports = Object.freeze({
+    UserAlreadyExists: "User with this identifiers (username/phoneNumber/email) is already exists.",
+    UserCreated: "User created successfully."
+});
